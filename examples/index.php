f69d2ca4ed4d33c89526f030df4878fa82e770b7
Remy <remy.sainsard@gmail.com>
2021-04-05 17:16:16 +0200

    re-order project in 'examples' folder

--- examples/index.php
+++ examples/index.php
@@ -1,15 +1,17 @@
 <?php
 
-require './vendor/autoload.php';
+require './../vendor/autoload.php';
 
 use RemySd\SimpleRouter\Router;
 
 $router = new Router();
 
-$router->setBasePath('/simple-router');
+$router->setBasePath('/simple-router/examples');
 
 $router->addRoute('/', 'home', 'index', 'homepage');
 $router->addRoute('/articles', 'article', 'all', 'article_all');
 $router->addRoute('/articles/{id}', 'article', 'show', 'article_single');
 
 $properties = $router->match($_SERVER['REQUEST_URI']);
+
+var_dump($properties);
